17eaaa4f8f7e1f595d1f685946d59dbcbcdd2a2d
Drobertson1990 <Dmrfencing@hotmail.com>
2026-02-22 15:14:16 -0700

    Implement database models for health tracking
    Added classes for Dose, Meal, and Workout to manage health tracking data.

--- database.py
+++ database.py
@@ -1,1 +1,37 @@
+from sqlalchemy import create_engine, Column, Integer, String, Float, Date
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import datetime
 
+engine = create_engine("sqlite:///tracker.db")
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+
+class Dose(Base):
+    __tablename__ = "doses"
+    id = Column(Integer, primary_key=True)
+    compound = Column(String)
+    amount_mg = Column(Float)
+    date = Column(Date, default=datetime.date.today)
+
+class Meal(Base):
+    __tablename__ = "meals"
+    id = Column(Integer, primary_key=True)
+    meal_name = Column(String)
+    calories = Column(Integer)
+    protein = Column(Integer)
+    carbs = Column(Integer)
+    fats = Column(Integer)
+    date = Column(Date, default=datetime.date.today)
+
+class Workout(Base):
+    __tablename__ = "workouts"
+    id = Column(Integer, primary_key=True)
+    exercise = Column(String)
+    sets = Column(Integer)
+    reps = Column(Integer)
+    weight = Column(Float)
+    date = Column(Date, default=datetime.date.today)
+
+def init_db():
+    Base.metadata.create_all(engine)
